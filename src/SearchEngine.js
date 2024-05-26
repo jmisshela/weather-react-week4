a5aa1fe362ec67bbf52002a1afd3327c8b0f0c58
jmisshela <138009686+jmisshela@users.noreply.github.com>
2024-05-26 21:27:12 +0800

    Added State variables & added Axios

--- src/SearchEngine.js
+++ src/SearchEngine.js
@@ -1,11 +1,21 @@
-import React from "react";
+import React, { useState } from "react";
+import axios from "axios";
 
 export default function SearchEngine() {
+  const [city, setCity] = useState("");
+
+  function displayWeather(response) {}
+
   function handleSubmit(event) {
     event.prevemtDefault();
+    let apiKey = "97c2f6a3b34509ac62090edc5d18d949";
+    let apiUrl = `https://api.openweathermap.org/data/2.5/weather?q=${city}&appid=${apiKey}&units=metric`;
+    axios.get(apiUrl).then(displayWeather);
   }
 
-  function updateCity(event) {}
+  function updateCity(event) {
+    setCity(event.target.value);
+  }
 
   return (
     <form onSubmit={handleSubmit}>
